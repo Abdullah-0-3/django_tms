f58cf003850c712e22d68cad7cbb034bb90103c0
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-10-31 11:04:45 +0500

    feat: Url, Setting for Core TMS

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Task Management System")
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('auth/', include('auth.urls')),
+    path('organization/', include('organization.urls')),
 ]
