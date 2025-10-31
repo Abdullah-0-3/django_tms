551cac56af883da5f162f311b84efe3aa28916de
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-10-31 10:39:08 +0500

    feat: Models, Views, Urls for Auth

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from organization.models import Organization
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    organization = models.ForeignKey(
+        Organization, null=True, blank=True, on_delete=models.SET_NULL, related_name='users'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        org_name = self.organization.name if self.organization else "No Organization"
+        return f"{self.username} - {org_name}"
+
