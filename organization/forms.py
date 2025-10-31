26cecab684fce763448ac540edb5ca2cce8b9bae
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-10-31 10:12:37 +0500

    feat: Models & Forms for Organization

---
+++ organization/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Organization
+
+class OrganizationForm(forms.ModelForm):
+    class Meta:
+        model = Organization
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # created_by = 
+
+    def __str__(self):
+        return f"{self.name}"
