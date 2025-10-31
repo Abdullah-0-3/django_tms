7ba54f9bace859e3086ca9e49c1d3731f3cb9aa7
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-10-31 10:57:33 +0500

    feat: Views, Logic Changes, Forms for Auth

--- auth/models.py
+++ auth/models.py
@@ -1,15 +1,10 @@
 from django.db import models
-from organization.models import Organization
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    organization = models.ForeignKey(
-        Organization, null=True, blank=True, on_delete=models.SET_NULL, related_name='users'
-    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        org_name = self.organization.name if self.organization else "No Organization"
-        return f"{self.username} - {org_name}"
+        return self.username
 
--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 class CustomLoginView(LoginView):
     template_name = 'auth/login.html'
@@ -13,11 +13,11 @@ class CustomLoginView(LoginView):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('/')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'auth/register.html', {'form': form})
