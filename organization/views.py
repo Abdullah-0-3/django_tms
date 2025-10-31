bb726729282e2ef58407a2f441c70162a3415245
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-10-31 10:49:58 +0500

    feat: Views, Url, Models, Admin for Organization

--- organization/views.py
+++ organization/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Organization
+from .forms import OrganizationForm
 
-# Create your views here.
+class OrganizationCreateView(LoginRequiredMixin, CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'organization/create.html'
+    success_url = reverse_lazy('organization_list')
+    
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class OrganizationListView(LoginRequiredMixin, ListView):
+    model = Organization
+    template_name = 'organization/list.html'
+    context_object_name = 'organizations'
