40ca64f56897cfd7fbff78ad35249c6dc298d2c9
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-10-31 11:13:08 +0500

    feat: frontend config

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Task Management System")
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('', home, name='home'),
