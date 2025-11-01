a402034aca99cc14739dcd50ffd1c63427e6841a
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-11-01 11:40:54 +0500

    feat: Settings & Urls for Core

--- core/urls.py
+++ core/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('organization/', include('project.urls')),
     path('organization/', include('sprint.urls')),
     path('organization/', include('task.urls')),
+    path('organization/', include('sprint.urls')),
 ]
