726803cfc9b709fccbe957abf56b1f02a19e1039
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2025-11-01 10:20:54 +0500

    feat: Models for task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from project.models import Project
 
-# Create your models here.
+class Task(models.Model):
+
+    TASK_PRIORITY = (
+        ("h", "High"),
+        ("m", "Medium"),
+        ("l", "Low"),
+    )
+
+    TASK_STAGE = (
+        ('to do', "To Do"),
+        ('in progress', 'In Progress'),
+        ('done', 'Done'),
+    )
+
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name='tasks'
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    task_stage = models.CharField(max_length=20, choices=TASK_STAGE, default='to do')
+    task_priority = models.CharField(max_length=1, choices=TASK_PRIORITY, default='m')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        'accounts.User', related_name='created_tasks', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.project.title}"
+
+class Comment(models.Model):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='comments'
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        'accounts.User', related_name='created_comments', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.task.title}"
